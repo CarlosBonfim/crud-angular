2e51abccd7ac139ae341bff01673bd108824f7c4
CarlosBonfim <brunobonfim10@hotmail.com>
2020-12-17 22:45:31 -0400

    inserção de funcionalidade

--- frontend/src/app/components/product/product.service.ts
+++ frontend/src/app/components/product/product.service.ts
@@ -1,20 +1,24 @@
-import { Injectable } from '@angular/core';
-import {MatSnackBar} from '@angular/material/snack-bar';
+import { Product } from "./product.model";
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { MatSnackBar } from "@angular/material/snack-bar";
+import { Observable } from "rxjs";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class ProductService {
+  baserUrl = "http://localhost:3001/products";
+  constructor(private snackBar: MatSnackBar, private http: HttpClient) {}
 
-  constructor(private snackBar: MatSnackBar) { }
-
-  showMessage(msg: string):void {
-    this.snackBar.open(msg, 'X', {
-        duration: 3000,
-        horizontalPosition: "right",
-        verticalPosition: "top"
-    })
-    
+  showMessage(msg: string): void {
+    this.snackBar.open(msg, "X", {
+      duration: 3000,
+      horizontalPosition: "right",
+      verticalPosition: "top",
+    });
   }
-  
+  create(product: Product): Observable<Product> {
+    return this.http.post<Product>(this.baserUrl, product);
+  }
 }
