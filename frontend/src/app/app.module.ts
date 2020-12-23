58537317939bea0536936bc0c5e866183661f5e1
CarlosBonfim <brunobonfim10@hotmail.com>
2020-12-22 22:28:02 -0400

    novos imports no modulo

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
+import { NgModule, LOCALE_ID } from "@angular/core";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
@@ -26,10 +26,17 @@ import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatInputModule } from "@angular/material/input";
 import {HttpClientModule} from "@angular/common/http";
 import { ProductReadComponent } from './components/product/product-read/product-read.component';
-import { ProductRead2Component } from './components/product/product-read2/product-read2.component';
+// import { ProductRead2Component } from './components/product/product-read2/product-read2.component';
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
+import { umask } from "process";
+
+import localePt from '@angular/common/locales/pt'
+import { registerLocaleData } from '@angular/common';
+import { ProductUpdateComponent } from './components/product/product-update/product-update.component'
+
+registerLocaleData(localePt);
 
 @NgModule({
   declarations: [
@@ -43,7 +50,8 @@ import { MatSortModule } from '@angular/material/sort';
     ForDirective,
     ProductCreateComponent,
     ProductReadComponent,
-    ProductRead2Component,
+    ProductUpdateComponent,
+    // ProductRead2Component,
   ],
   imports: [
     BrowserModule,
@@ -64,7 +72,10 @@ import { MatSortModule } from '@angular/material/sort';
     MatSortModule
     
   ],
-  providers: [],
+  providers: [{
+    provide: LOCALE_ID,
+    useValue: 'pt-BR'
+  }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
