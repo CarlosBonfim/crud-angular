37b6549927a16184b35dbbe3daf21f12515d4a3d
CarlosBonfim <brunobonfim10@hotmail.com>
2020-12-23 23:20:58 -0400

    Adicionadas novas funcionalidades usando headerService

--- frontend/src/app/views/product-crud/product-crud.component.ts
+++ frontend/src/app/views/product-crud/product-crud.component.ts
@@ -1,3 +1,4 @@
+import { HeaderService } from './../../components/template/footer/header.service';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router'
 
@@ -8,7 +9,20 @@ import { Router } from '@angular/router'
 })
 export class ProductCrudComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private headerService: HeaderService) { 
+    headerService.headerData = {
+      title:'Cadastro de Produtos',
+      icon: 'storefront',
+      routeUrl: '/products'
+    }
+  }
+
+
+// export class ProductCrudComponent implements OnInit {
+
+//   constructor(private router: Router) { 
+
+//   }
 
   ngOnInit(): void {
   }
